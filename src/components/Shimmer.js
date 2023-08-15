edd5755a85e28f5f96b2327e2696cc4a98a6a7e2
Rajashree Rajesh Arvikar <79647432+rajashreearvikar19@users.noreply.github.com>
2023-08-15 13:40:08 +0530

    Add files via upload

---
+++ src/components/Shimmer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Shimmer.css"; 
+
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-line"></div>
+      <div className="shimmer-line"></div>
+      <div className="shimmer-line"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
